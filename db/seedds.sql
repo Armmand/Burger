545e67af022570339c8e64aa2b96d83a52f3fbdc
Armmand Alston <ArmmandAlston33@gmail.com>
2017-05-28 21:29:18 -0400

    Added lines of code

--- db/seedds.sql
+++ db/seedds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Jordan Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES('MushroomTay Pay Burger');
 INSERT INTO burgers (burger_name) VALUES('Kobe Burger');
